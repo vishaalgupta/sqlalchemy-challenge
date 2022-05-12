1300d32613d73713137553e5a170a391ae2e9adc
Vishaal Gupta <vishaalgupta95@gmail.com>
2022-05-11 21:18:48 -0700

    Flask API

--- app.py
+++ app.py
@@ -84,6 +84,80 @@ def stations():
 
     return jsonify(all_stations)
 
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Temperature"""
+    
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= '2016-08-23').\
+        filter(Measurement.date <= '2017-08-23').\
+        filter(Measurement.station == 'USC00519281').all()
+
+    session.close()
+
+    all_temp = []
+    for date, temp in results:
+        temp_dict = {}
+        temp_dict[date] = temp
+        all_temp.append(temp_dict)
+
+    return jsonify(all_temp)
+
+@app.route("/api/v1.0/<start>")
+def temp_start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Temperature Summary"""
+    
+    if (start <= '2017-08-23'):
+        results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+            filter(Measurement.date >= start).all()
+        
+        all_temp = []
+        for min, max, avg in results:
+            temp_dict = {}
+            temp_dict["min"] = min
+            temp_dict["max"] = max
+            temp_dict["avg"] = avg
+            all_temp.append(temp_dict)
+           
+        return jsonify(all_temp)
+       
+    else:
+        return jsonify({"error": f"Start date out of range."}), 404   
+
+    session.close()
+
+@app.route("/api/v1.0/<start>/<end>")
+def temp_summary(start, end):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Temperature Summary"""
+    
+    if ((start <= '2017-08-23') & (end >= '2010-01-01') & (start < end)):
+        results = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+        
+        all_temp = []
+        for min, max, avg in results:
+            temp_dict = {}
+            temp_dict["min"] = min
+            temp_dict["max"] = max
+            temp_dict["avg"] = avg
+            all_temp.append(temp_dict)
+           
+        return jsonify(all_temp)
+       
+    else:
+        return jsonify({"error": f"Start or end date out of range."}), 404   
+
+    session.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
